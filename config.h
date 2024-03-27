b763be5c7936e34ec3ee84941c4f82507d9c7fb4
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2024-03-27 15:18:00 +0100

    print syscalls by default

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define REWRITE_TO_ZPOLINE      1
-#define PRINT_SYSCALLS          0
+#define PRINT_SYSCALLS          1
 #define SAVE_VECTOR_REGS        1
 #define RETURN_IMMEDIATELY      0
 #define COMPAT_NONDEP_APP       0
